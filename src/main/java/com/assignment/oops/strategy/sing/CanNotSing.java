e829bee891a78d40330a0186aa11047cdc7004d2
Naresh <nareshkumarit@gmail.com>
2018-10-08 20:26:16 +0800

    Completed section B

--- src/main/java/com/assignment/oops/strategy/sing/CanNotSing.java
+++ src/main/java/com/assignment/oops/strategy/sing/CanNotSing.java
@@ -3,7 +3,6 @@ package com.assignment.oops.strategy.sing;
 import com.assignment.oops.strategy.Singable;
 
 public class CanNotSing implements Singable {
-
     @Override
     public String sing() {
         return "Can not sing";
